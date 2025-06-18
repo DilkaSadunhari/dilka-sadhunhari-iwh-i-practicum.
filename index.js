d4f439c56d72358e1dca3cfd4dd228a075c12e3e
Dilka sadunhari <sadunharidilka@gmailcom>
2025-06-18 11:28:47 +0530

    First commit to my Integrating With HubSpot I: Foundations practicum repository.
    pug form created
    
    homepage and updates done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,19 +10,62 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const movies = 'https://api.hubspot.com/crm/v3/objects/movies?properties=name,director,movie_genre,movie_release_year';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(movies, { headers });
+        const data = resp.data.results;
+       // res.json(data); 
+
+        res.render('homepage', { title: 'Home | Integrating With HubSpot I Practicum', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newMovie = {
+        properties: {
+            "name": req.body.name,
+            "director": req.body.director,
+            "movie_genre": req.body.movie_genre,
+            "movie_release_year": req.body.movie_release_year
+        }
+    };
+
+    const createMovieURL = `https://api.hubapi.com/crm/v3/objects/movies`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.post(createMovieURL, newMovie, { headers });
+       res.redirect('/');
+    } catch (err) {
+        console.error('Error creating movie:', err.response?.data || err.message);
+        res.status(500).send('Failed to create movie');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -65,7 +110,6 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
